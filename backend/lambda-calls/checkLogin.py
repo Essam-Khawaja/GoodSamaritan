0334a1ed15b9d8b874b61f05eda2c23aa2be861c
HavocOmega <80192800+HavocOmega@users.noreply.github.com>
2025-11-08 21:14:26 -0700

    feat: check login function with json creation

--- backend/lambda-calls/checkLogin.py
+++ backend/lambda-calls/checkLogin.py
@@ -5,7 +5,22 @@ import boto3
 client = boto3.client("lambda")
 
 
-def check_login(user_id):
-    # TODO: Implement the check_login function
+def check_login(email, password):
+    payload = json.dumps({"email": email, "password": password})
 
-    return
+    response = client.invoke(FunctionName="checkLogin", Payload=payload)
+
+    if response["StatusCode"] == 200:
+        payload = json.dumps({"email": email})
+        response = client.invoke(FunctionName="getUser", Payload=payload)
+
+        response_data = json.loads(response["Payload"].read().decode("utf-8"))
+        body = json.loads(response_data.get("body", "{}"))
+
+        user_data = body.get("user")
+
+        with open("user.json", "w") as file:
+            json.dump(user_data, file, indent=2)
+
+
+check_login("testtest@gmail.com", "abcdef")
