16941e14ab0ccbea1beb476f3e609ded270cf00a
HavocOmega <80192800+HavocOmega@users.noreply.github.com>
2025-11-08 22:47:32 -0700

    chore: task manager setup and todo

---
+++ backend/managers/orgs/taskManager.py
@@ -0,0 +1,3 @@
+# TODO: Make all necessary task management functions public and connect them to
+# the task manager for ease of use via taskmanager.createTask() for example
+# also handles org data handling to send the necessary data to the backend
